bfaac118555d2e3c974a9822722443ef54e27d93
Sumit Kakshapati <me.summet@gmail.com>
2024-03-05 17:08:47 +0545

    ancroid package updated

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,16 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_native_contact_picker/flutter_native_contact_picker.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
   @override
-  _MyAppState createState() => _MyAppState();
+  State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  final FlutterContactPicker _contactPicker = new FlutterContactPicker();
+  final FlutterContactPicker _contactPicker = FlutterContactPicker();
   List<Contact>? _contacts;
 
   @override
@@ -21,32 +23,22 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Contact Picker Example App'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Contact Picker Example App'),
         ),
-        body: new Center(
-          child: new Column(
+        body: Center(
+          child: Column(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              new MaterialButton(
+              MaterialButton(
                 color: Colors.blue,
-                child: new Text("Single"),
+                child: const Text("Single"),
                 onPressed: () async {
                   Contact? contact = await _contactPicker.selectContact();
                   setState(() {
                     _contacts = contact == null ? null : [contact];
-                  });
-                },
-              ),
-              new MaterialButton(
-                color: Colors.blue,
-                child: new Text("Multiple"),
-                onPressed: () async {
-                  final contacts = await _contactPicker.selectContacts();
-                  setState(() {
-                    _contacts = contacts;
                   });
                 },
               ),
